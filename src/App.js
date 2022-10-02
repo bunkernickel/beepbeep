b95e30d782f51503349b9f0f950cf7c80bc8909f
bunkernickel <cbizjak21@jcu.edu>
2022-10-02 19:08:52 -0400

    real first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './download.jpeg';
 import './App.css';
 
 function App() {
@@ -7,18 +7,34 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          NILProject
+          NILProject development server
         </p>
         <div>
-          This is a barebones React app.
+          Links to our sites:
         </div>
+        <a
+          className="App-link"
+          href="https://drive.google.com/drive/folders/1JTyFjwYsAKY4JwzCAeN94vbVGYR-pjHD"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shared folder
+        </a>
+        <a
+          className="App-link"
+          href="https://app.asana.com/0/1202927509660694/1203010425889848"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Asana
+        </a>
         <a
           className="App-link"
           href="https://www.youtube.com/watch?v=hQAHSlTtcmY&ab_channel=WebDevSimplified"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          react lol ( eventual gate to demo site arch )
         </a>
       </header>
     </div>
